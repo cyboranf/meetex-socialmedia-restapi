78ba2b2ae48c67c15a551a132bfb9363568d0045
cyboranf <cyboranf@gmail.com>
2023-03-01 18:16:10 +0100

    Project -opened

---
+++ src/main/java/com/example/meetexApi/MeetexApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.meetexApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetexApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetexApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meetexApi/MeetexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meetexApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

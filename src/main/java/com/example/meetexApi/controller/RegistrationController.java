569cb19e9aa6a9f6298bbe8eeb9f6e3cc4623d29
cyboranf <cyboranf@gmail.com>
2023-03-12 19:45:38 +0100

    post method to create an user -added

--- src/main/java/com/example/meetexApi/controller/RegistrationController.java
+++ src/main/java/com/example/meetexApi/controller/RegistrationController.java
@@ -1,13 +1,43 @@
 package com.example.meetexApi.controller;
 
+import com.example.meetexApi.model.User;
 import com.example.meetexApi.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegistrationController {
     private final UserService userService;
 
-    public RegistrationController(UserService userService) {
+    private final PasswordEncoder passwordEncoder;
+
+    public RegistrationController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/register")
+    public String createAnAccount(@Validated @ModelAttribute("user") User user,
+                                  BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("user", user);
+            return "register";
+        }
+        if (userService.findByEmail(user.getEmail()) != null) {
+            return "This email is already used";
+        }
+        if (!user.getPassword().equals(user.getMatchingPassword())) {
+            return "Passwords are not the same";
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userService.save(user);
+
+        return "user";
+
     }
 }

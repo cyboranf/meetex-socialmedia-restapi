bf43db385388d1ef9f93a47305f91169146e334b
cyboranf <cyboranf@gmail.com>
2023-03-21 18:10:01 +0100

    inits class -added

---
+++ src/main/java/com/example/meetexApi/init/CommunityInitClass.java
@@ -0,0 +1,28 @@
+package com.example.meetexApi.init;
+
+import com.example.meetexApi.model.Community;
+
+import com.example.meetexApi.service.CommunityService;
+
+public class CommunityInitClass {
+    private final CommunityService communityService;
+
+    public CommunityInitClass(CommunityService communityService) {
+        this.communityService = communityService;
+    }
+
+    public void initPages() {
+        Community comNo1 = new Community();
+        Community comNo2 = new Community();
+        Community comNo3 = new Community();
+
+        comNo1.setName("Script House");
+        comNo2.setName("Phoenix Team");
+        comNo3.setName("Java Developers");
+
+        communityService.save(comNo1);
+        communityService.save(comNo2);
+        communityService.save(comNo3);
+    }
+
+}

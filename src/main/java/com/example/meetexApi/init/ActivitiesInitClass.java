210548ca21f3246103e2646e12d98b11ffae7a46
cyboranf <cyboranf@gmail.com>
2023-03-21 18:06:17 +0100

    inits class -added

---
+++ src/main/java/com/example/meetexApi/init/ActivitiesInitClass.java
@@ -0,0 +1,38 @@
+package com.example.meetexApi.init;
+
+import com.example.meetexApi.model.Activities;
+import com.example.meetexApi.service.ActivitiesService;
+import com.example.meetexApi.service.UserService;
+
+public class ActivitiesInitClass {
+    private final ActivitiesService activitiesService;
+    private final UserService userService;
+
+    public ActivitiesInitClass(ActivitiesService activitiesService, UserService userService) {
+        this.activitiesService = activitiesService;
+        this.userService = userService;
+    }
+
+    public void initActivities(){
+        Activities activitiesNo1=new Activities();
+        Activities activitiesNo2=new Activities();
+        Activities activitiesNo3=new Activities();
+        Activities activitiesNo4=new Activities();
+        Activities activitiesNo5=new Activities();
+        Activities activitiesNo6=new Activities();
+
+        activitiesNo1.setName("Hiking");
+        activitiesNo2.setName("Travelling");
+        activitiesNo3.setName("Cooking");
+        activitiesNo4.setName("Rock and Roll");
+        activitiesNo5.setName("Hockey");
+        activitiesNo6.setName("Basket");
+
+        activitiesService.save(activitiesNo1);
+        activitiesService.save(activitiesNo2);
+        activitiesService.save(activitiesNo3);
+        activitiesService.save(activitiesNo4);
+        activitiesService.save(activitiesNo5);
+        activitiesService.save(activitiesNo6);
+    }
+}
---
+++ src/main/java/com/example/meetexApi/init/PagesInitClass.java
@@ -0,0 +1,29 @@
+package com.example.meetexApi.init;
+
+import com.example.meetexApi.model.Pages;
+import com.example.meetexApi.service.PagesService;
+import com.example.meetexApi.service.UserService;
+
+public class PagesInitClass {
+    private final PagesService pagesService;
+    private final UserService userService;
+
+    public PagesInitClass(PagesService pagesService, UserService userService) {
+        this.pagesService = pagesService;
+        this.userService = userService;
+    }
+
+    public void initPages() {
+        Pages pageNo1 = new Pages();
+        Pages pageNo2 = new Pages();
+        Pages pageNo3 = new Pages();
+
+        pageNo1.setName("Script House");
+        pageNo2.setName("Phoenix Team");
+        pageNo3.setName("Java Developers");
+
+        pagesService.save(pageNo1);
+        pagesService.save(pageNo2);
+        pagesService.save(pageNo3);
+    }
+}

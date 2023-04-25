29c8addce478f1e1f962aa8aa48cdbe306db35dd
cyboranf <cyboranf@gmail.com>
2023-04-25 21:08:20 +0200

    seeds -commented

--- src/main/java/com/example/meetexApi/seed/ActivitiesInitClass.java
+++ src/main/java/com/example/meetexApi/seed/ActivitiesInitClass.java
@@ -1,38 +1,38 @@
-package com.example.meetexApi.seed;
-
-import com.example.meetexApi.model.Activities;
-import com.example.meetexApi.service.ActivitiesService;
-import com.example.meetexApi.service.UserService;
-
-public class ActivitiesInitClass {
-    private final ActivitiesService activitiesService;
-    private final UserService userService;
-
-    public ActivitiesInitClass(ActivitiesService activitiesService, UserService userService) {
-        this.activitiesService = activitiesService;
-        this.userService = userService;
-    }
-
-    public void initActivities(){
-        Activities activitiesNo1=new Activities();
-        Activities activitiesNo2=new Activities();
-        Activities activitiesNo3=new Activities();
-        Activities activitiesNo4=new Activities();
-        Activities activitiesNo5=new Activities();
-        Activities activitiesNo6=new Activities();
-
-        activitiesNo1.setName("Hiking");
-        activitiesNo2.setName("Travelling");
-        activitiesNo3.setName("Cooking");
-        activitiesNo4.setName("Rock and Roll");
-        activitiesNo5.setName("Hockey");
-        activitiesNo6.setName("Basket");
-
-        activitiesService.save(activitiesNo1);
-        activitiesService.save(activitiesNo2);
-        activitiesService.save(activitiesNo3);
-        activitiesService.save(activitiesNo4);
-        activitiesService.save(activitiesNo5);
-        activitiesService.save(activitiesNo6);
-    }
-}
+//package com.example.meetexApi.seed;
+//
+//import com.example.meetexApi.model.Activity;
+//import com.example.meetexApi.service.ActivitiesService;
+//import com.example.meetexApi.service.UserService;
+//
+//public class ActivitiesInitClass {
+//    private final ActivitiesService activitiesService;
+//    private final UserService userService;
+//
+//    public ActivitiesInitClass(ActivitiesService activitiesService, UserService userService) {
+//        this.activitiesService = activitiesService;
+//        this.userService = userService;
+//    }
+//
+//    public void initActivities(){
+//        Activity activityNo1 =new Activity();
+//        Activity activityNo2 =new Activity();
+//        Activity activityNo3 =new Activity();
+//        Activity activityNo4 =new Activity();
+//        Activity activityNo5 =new Activity();
+//        Activity activityNo6 =new Activity();
+//
+//        activityNo1.setName("Hiking");
+//        activityNo2.setName("Travelling");
+//        activityNo3.setName("Cooking");
+//        activityNo4.setName("Rock and Roll");
+//        activityNo5.setName("Hockey");
+//        activityNo6.setName("Basket");
+//
+//        activitiesService.save(activityNo1);
+//        activitiesService.save(activityNo2);
+//        activitiesService.save(activityNo3);
+//        activitiesService.save(activityNo4);
+//        activitiesService.save(activityNo5);
+//        activitiesService.save(activityNo6);
+//    }
+//}

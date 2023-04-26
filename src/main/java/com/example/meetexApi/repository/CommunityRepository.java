b046acbc8ac92d242c34c7f5a286e1f86cc3892a
cyboranf <cyboranf@gmail.com>
2023-04-26 15:57:31 +0200

    CommunityRepository class -added

--- src/main/java/com/example/meetexApi/repository/CommunityRepository.java
+++ src/main/java/com/example/meetexApi/repository/CommunityRepository.java
@@ -4,5 +4,4 @@ import com.example.meetexApi.model.Community;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommunityRepository extends JpaRepository<Community, Long> {
-
 }

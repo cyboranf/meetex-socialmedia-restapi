bd8e9b93adffdfcf792d64189559db74ebb12976
cyboranf <cyboranf@gmail.com>
2023-07-14 15:43:26 +0200

    validator, mapper and exceptions for Comment - added

--- src/main/java/com/example/meetexApi/mapper/CommentMapper.java
+++ src/main/java/com/example/meetexApi/mapper/CommentMapper.java
@@ -9,7 +9,7 @@ import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface CommentMapper {
     CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
 
@@ -21,7 +21,7 @@ public interface CommentMapper {
     @Mapping(target = "sendDate", ignore = true)
     @Mapping(target = "sender", ignore = true)
     @Mapping(target = "post", ignore = true)
-    @Mapping(target = "parentComments", ignore = true)
+    @Mapping(target = "parentComment", ignore = true)
     @Mapping(target = "childComments", ignore = true)
     Comment commentRequestDTOtoComment(CommentRequestDTO commentRequestDTO);
 
